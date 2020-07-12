a3e8a94e148c8673e54d35a601176c0082b053d6
Aravind A <aravind.arali@gmail.com>
2020-07-12 15:07:26 +0530

    added wwwhisper addon heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,12 @@ const path = require('path');
 const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "..", "public");
+const wwwhisper = require("connect-wwwhisper");
+
+app.use(wwwhisper());
+
 const port = process.env.PORT || 3000;
+
 
 app.use(express.static(publicPath));
 
